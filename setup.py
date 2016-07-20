0d3c534317d14c38350918bac9e211347d7c6744
Ashish <ashish@unbreakables-MacBook-Air.local>
2016-07-20 19:58:11 +0530

    changed description

--- setup.py
+++ setup.py
@@ -3,11 +3,9 @@ setup(
   name = 'artifacia',
   packages = ['artifacia'],
   version = '0.1',
-  description = 'A python API client to use Artifacia's API',
+  description = "A python client to integrate visual recommendation feature from Artifacia API.",
   author = 'Ashish Kumar',
   author_email = 'erashish122@gmail.com',
   url = 'https://github.com/artifacia/artifacia-client-python',
-  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', # I'll explain this in a second
-  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
-  classifiers = [],
+  keywords = ['pyartifacia', 'artifaciapy', 'pythonartifacia', 'recommendation', 'artifacia']
 )
