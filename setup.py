dbb512c0c97d470ec575b62d3a2fa7070dd632bd
Ashish <ashish@unbreakables-MacBook-Air.local>
2016-07-26 21:06:42 +0530

    changed end point

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'artifacia',
   packages = ['artifacia'],
-  version = '0.1.0',
+  version = '0.1.1',
   description = "A python client to integrate visual recommendation feature from Artifacia API.",
   author = 'Ashish Kumar',
   author_email = 'erashish122@gmail.com',
