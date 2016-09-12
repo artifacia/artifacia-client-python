dc3f696585fc32ddb1823e3654521adc83a384b1
Ashish <ashish@unbreakables-MacBook-Air.local>
2016-09-12 13:39:38 +0530

    change authentication type

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'artifacia',
   packages = ['artifacia'],
-  version = '0.1.4',
+  version = '0.1.5',
   description = "A python client to integrate Visual Discovery feature from Artifacia API.",
   author = 'Ashish Kumar',
   author_email = 'erashish122@gmail.com',
