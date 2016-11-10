41f11a6c979ddf4ebb03ffbd89f1b726248e52cc
Vivek <vivek@snapshopr.co>
2016-11-10 22:00:30 +0530

    increment the version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'artifacia',
   packages = ['artifacia'],
-  version = '0.1.7.1',
+  version = '0.1.7.2',
   description = "A python client to integrate Visual Discovery feature from Artifacia API.",
   author = 'Ashish Kumar',
   author_email = 'erashish122@gmail.com',
